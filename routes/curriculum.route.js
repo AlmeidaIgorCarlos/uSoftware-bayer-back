d398a3e998724df793a57342097ccbd5c5c1efe2
Gabriel Gomes <gabrielsousagomes@hotmail.com>
2019-09-29 22:21:58 -0300

    implementacao de curriculum quase finalizada

---
+++ routes/curriculum.route.js
@@ -0,0 +1,84 @@
+const curriculumSchema = require('./../schemas/curriculum.shema')
+const curriculumService = require('../services/curriculum.service')
+const authService = require('./../services/auth.service')
+
+const {Validator} = require('express-json-validator-middleware');
+const validate = new Validator({allErrors: true}).validate
+
+module.exports = app => {
+    app.post('/curriculum', validate({body: curriculumSchema()}), authService.authorize('user'), async (req, res)=>{
+        try {
+            const curriculum = req.body
+            const savedCurriculum = await curriculumService.saveCurriculumInDatabase(curriculum)
+            res.status(200).send({
+                message: 'Curriculum saved successfully',
+                curriculum: {...savedCurriculum[0]}
+            })
+        }catch (error) {
+            res.status(400).send({message: error.message})
+        }finally{
+            res.end()
+        }
+    })
+
+    app.put('/recruiter', validate({body: curriculumSchema()}), authService.authorize('user'), async (req, res)=>{
+        try {
+            const curriculum = req.body
+            await curriculumService.updateCurriculumInDatabase(curriculum)
+            res.status(200).send({
+                message: 'Curriculum updated successfully'
+            })
+        }catch (error) {
+            res.status(400).send({message: error.message})
+        }finally{
+            res.end()
+        }
+    })
+
+    app.delete('/curriculum', authService.authorize('user'), async (req, res)=>{
+        try {
+            const curriculum = req.body
+            await curriculumService.deleteCurriculumFromDatabase(curriculum)
+            res.status(200).send({
+                message: 'curriculum deleted successfully'
+            })
+        }catch (error) {
+            res.status(400).send({message: error.message})
+        }finally{
+            res.end()
+        }
+    })
+
+    app.get('/curriculum/:id',async (req, res) => {
+        try {
+            let curriculum = {curriculum_id: req.params.id || 0}
+            const curriculums = await curriculumService.getCurriculumFromDatabase(curriculum)
+
+            res
+                .status(200)
+                .send({ 'curriculum': curriculums[0] })
+        } catch (error) {
+            res
+                .status(500)
+                .send({
+                    'message': error.message
+                })
+        } finally {
+            res.end()
+        }
+    })
+
+    app.get('/curriculum', async (req, res) => {
+        try {
+            let curriculums = await curriculumService.getCurriculumFromDatabase()
+            res.status(200).send({
+                message: "successful curriculums",
+                recruiters: curriculums
+            })
+        } catch (error) {
+            res.status(500).send({ message: error.message })
+        } finally {
+            res.end()
+        }
+    })
+}
---
+++ services/curriculum.service.js
@@ -0,0 +1,64 @@
+let curriculumsDatabase = new require('../repository/curriculum.repository')
+curriculumDatabase = new curriculumsDatabase()
+
+module.exports = {
+    async saveCurriculumInDatabase (curriculum) {
+        curriculum.isActive = 1
+        const isCurriculumSaved = await this.checkIfCurriculumIsInDabase(curriculum)
+        if(isCurriculumSaved)
+            throw new Error('The curriculum provided is already saved into the database')
+
+        await curriculumDatabase.insert(curriculum)
+        const savedCurriculum = await curriculumDatabase.select(curriculum)
+        return savedCurriculum
+    },
+    async updateCurriculumInDatabase (curriculum) {
+        // const isRecruiterIsInDabaseWithSameEmail = await this.checkIfRecruiterIsInDabaseWithSameEmail(recruiter)
+
+        // if(!isRecruiterIsInDabaseWithSameEmail)
+        //     throw new Error('Recruiter is not saved in the application')
+
+        // const databaseRecruiter = await this.getRecruiterFromDatabase(recruiter)
+        // recruiter.recruiter_id = databaseRecruiter[0].recruiter_id
+
+        if(curriculum.recruiter_id == undefined)
+            throw new Error('curriculum_id is necessary to update a curriculum')
+
+        const updatedCurriculum = await curriculumDatabase.update(curriculum)
+        if(updatedCurriculum < 1)
+            throw new Error('None curriculum was updated')
+
+        return updatedCurriculum
+    },
+    async deleteCurriculumFromDatabase (curriculum) {
+        if(curriculum.recruiter_id == undefined)
+            throw new Error('curriculum_id is necessary to update a curriculum')
+
+        curriculum.isActive = 0
+
+        const updatedCurriculum = await curriculumDatabase.update(curriculum)
+        if(updatedCurriculum < 1)
+            throw new Error('None curriculum was deleted')
+
+        return updatedCurriculum
+    },
+    async checkIfCurriculumIsInDabase(curriculum){
+        const databaseCurriculums = await this.getCurriculumFromDatabase(curriculum)
+        let isCurriculumSaved = 0
+
+        databaseCurriculums.forEach(databaseCurriculum=>{
+            if(curriculum.name === databaseCurriculum.name)
+            isCurriculumSaved++
+        })
+
+        if(isCurriculumSaved > 0)
+            return true
+        else
+            return false
+
+    },
+    async getCurriculumFromDatabase (curriculum) {
+        const databaseCurriculum = await curriculumDatabase.select(curriculum)
+        return databaseCurriculum
+    }
+}
