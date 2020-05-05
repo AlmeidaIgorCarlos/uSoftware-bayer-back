5bd60098fc0645e8d282267dcf8a891cdcad2407
AlmeidaIgorCarlos <almeidaigorcarlos@gmail.com>
2020-05-05 20:22:07 -0300

    user module and repository

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import {TypeOrmModule} from '@nestjs/typeorm'
 import { UserModule } from './user/user.module';
 import ormConfig from '../ormconfig'
+import { user } from './entities/user';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(ormConfig),
+    TypeOrmModule.forFeature([user]),
     UserModule
   ],
   controllers: [],
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { user } from 'src/entities/user';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @InjectRepository(user)
+        readonly userRepository: Repository<user>
+    ) { }
+
+    getUserByEmail(email: string) {
+        return this.userRepository.findOne({
+            where: { email }
+        })
+    }
+
+}
