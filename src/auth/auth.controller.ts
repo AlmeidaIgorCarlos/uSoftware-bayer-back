be2d030e8a03e8d441c0480c9ba937fd8d641154
AlmeidaIgorCarlos <almeidaigorcarlos@gmail.com>
2020-05-07 23:40:22 -0300

    implementing jwt strategy passport

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,17 @@
-import { Controller, Post, Body, UseGuards, Request } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Request, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
-import { user } from 'src/user/user.entity';
 
 @Controller('auth')
 export class AuthController {
     constructor(
         readonly authService: AuthService
-    ){}
-    
+    ) { }
+
     @UseGuards(AuthGuard('local'))
     @Post('/login')
-    async login(@Request() req){
+    async login(@Request() req) {
         return this.authService.login(req.user)
     }
+
 }
---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,20 @@
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { Strategy, ExtractJwt } from 'passport-jwt'
+import { PassportStrategy } from '@nestjs/passport'
+import { jwtConstants } from "./constants";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(
+    ) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: jwtConstants.secret,
+        })
+    }
+
+    async validate(payload: any) {
+        return { ...payload }
+    }
+}
